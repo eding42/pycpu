6953f51baba3eae2d3d162c6f10a5b58fca55d35
Edward Ding <eding2019>
2019-03-18 22:07:36 -0700

    finish GHz readout support... now to add MHz back in

--- pycpu2.py
+++ pycpu2.py
@@ -8,7 +8,8 @@ import time
 command = input("Choose a function:\n1. CPU Frequency (MHz)\n2. CPU Temperature (UNDER DEVELOPEMENT)\n")
 
 if command == "1":
-    units = input("Choose unit:\n1.MHz\n2.GHz ")
-    while True:
-        print(round(psutil.cpu_freq()[0],-1)/1000)
-        time.sleep(1)
+    unit = input("Choose unit:\n1.MHz\n2.GHz ")
+    if unit == "2":
+        while True:
+            print(str(round(psutil.cpu_freq()[0],-1)/1000)+" GHz")
+            time.sleep(0.75)
