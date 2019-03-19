0585db6943db5d1f0d4635a85860cea0a7965ff6
Edward Ding <eding2019>
2019-03-18 22:12:16 -0700

    finish GHz and MHz distinctions, finish update interval selection

--- pycpu2.py
+++ pycpu2.py
@@ -8,13 +8,13 @@ import time
 command = input("Choose a function:\n1. CPU Frequency (MHz)\n2. CPU Temperature (UNDER DEVELOPEMENT)\n")
 
 if command == "1":
-    unit = input("Choose unit:\n1.MHz\n2.GHz ")
-    interval = int(input("Choose update interval (in seconds): "))
-    if unit == "1":
+    unit = input("Choose unit:\n1.MHz\n2.GHz\n")
+    interval = int(input("Choose update interval (in seconds):\n"))
+    if unit == "2":
         while True:
             print(str(round(psutil.cpu_freq()[0], -1) / 1000) + " GHz")
             time.sleep(interval)
-    if unit == "2":
+    if unit == "1":
         while True:
-            print(str(round(psutil.cpu_freq()[0],-1)/1000)+" GHz")
+            print(str(round(psutil.cpu_freq()[0],2))+" MHz")
             time.sleep(interval)
