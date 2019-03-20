9ab9977dee899be8a9a6149988b5df493d8e8333
Edward Ding <eding2019>
2019-03-19 22:23:17 -0700

    finish making program more resilient, will not break if entering string

--- pycpu2.py
+++ pycpu2.py
@@ -5,18 +5,25 @@ print("-------------- Welcome to PyCPU v1.0 --------------")
 import psutil
 import time
 
+# Made dummy variable as an example for a string, to compare input with it.
+dummy = "this is a string"
+
 command = input("Choose a function:\n1. CPU Frequency (MHz)\n2. CPU Temperature (UNDER DEVELOPEMENT)\n")
 
 if command == "1":
     while True:
         unit = int(input("\nChoose unit:\n1.MHz\n2.GHz\n"))
-        if type(unit)==str:
+        if unit.isdigit() == True:
+            unit = int(unit)
+        if type(unit)==type(dummy):
             print("\nSorry, please enter a supported value\n")
         else:
             break
     while True:
-        interval = float(input("\nChoose update interval (in seconds):\n"))
-        if type(interval)==str:
+        interval = input("\nChoose update interval (in seconds):\n")
+        if interval.isdigit() == True:
+            interval = int(interval)
+        if type(interval)==type(dummy):
             print("\nSorry, please enter a supported value")
         else:
             break
