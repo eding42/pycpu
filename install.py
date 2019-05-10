f0e487c2385d5870625d1a345a56289d0539110c
Edward Ding <eding2019@gmail.com>
2019-05-09 20:37:27 -0700

    fixed most of the installation errors on install.py

--- install.py
+++ install.py
@@ -1,9 +1,25 @@
 import os
+import subprocess
 
+# Abort function.
+def abort():
+   print("Aborting now...\n")
+   os._exit(0)
+
+# Checks for existing dependencies 
+print("\n--------------- Checking for Dependencies ---------------\n")
+
+# Checks for the setup-tools package
 try:
    import setuptools
 except ImportError:
    print("Please install the python3-setuptools package")
+
+# Checks for the installation of the python3-dev package.
+x=subprocess.call("dpkg -s python3-dev", shell=True)
+if x == 1:
+   print("\nYou still have to install python3-setuptools package. \n\nPlease install that with 'sudo apt install python3-dev'\n")
+   abort()
 
 # Clone & Install psutil
 
